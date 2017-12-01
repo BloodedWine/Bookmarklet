f5d661bd9f60fa0674a2567d644be3bc49b6589a
BloodedWine <caidenblock@gmail.com>
2017-12-01 08:07:06 -0600

    Test

--- shh.js
+++ shh.js
@@ -1,1 +1,9 @@
-var ting = prompt("Thing:"); var thing = ting.toUpperCase(); if (thing == "GMAIL") window.open("https://gmail.com","_self"); if (thing == "PEARSON") window.open("https://accounts.google.com/o/saml2/initsso?idpid=C00t0cavg&spid=701197821690&forceauthn=false","_self"); if (thing == "DONGLES") alert("iPhone 7 has many Dongles"); if( thing.indexOf('CAIDEN') >= 0)alert("Caiden was here"); if( thing.indexOf('JOEY') >= 0)alert("Joey was here"); if( thing.indexOf('JOEY','CAIDEN') >= 0)alert("Caiden and Joey were here"); else alert(ting);
+var ting = prompt("Thing:");
+var thing = ting.toUpperCase();
+if (thing == "GMAIL") window.open("https://gmail.com","_self");
+if (thing == "PEARSON") window.open("https://accounts.google.com/o/saml2/initsso?idpid=C00t0cavg&spid=701197821690&forceauthn=false","_self");
+if (thing == "DONGLES") alert("iPhone 7 has many Dongles");
+if( thing.indexOf('CAIDEN') >= 0)alert("Caiden was here");
+if( thing.indexOf('JOEY') >= 0)alert("Joey was here");
+if( thing.indexOf('CAIDEN') >= 0) && thing.indexOf('JOEY') >= 0)alert("Test was here");;
+else alert(ting);
