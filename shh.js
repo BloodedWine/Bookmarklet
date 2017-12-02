dc376744eaad87f851d945f1db27f9e89c27a1de
BloodedWine <caidenblock@gmail.com>
2017-12-01 19:10:43 -0600

    Update shh.js

--- shh.js
+++ shh.js
@@ -1,9 +1,21 @@
 var ting = prompt("Thing:");
 var thing = ting.toUpperCase();
+// Sites
 if (thing == "GMAIL") window.open("https://gmail.com","_self");
 if (thing == "PEARSON") window.open("https://accounts.google.com/o/saml2/initsso?idpid=C00t0cavg&spid=701197821690&forceauthn=false","_self");
+// Random
+if (thing == "REPLICATE")browser.bookmarks.create({
+  title: "THING",
+  url: "javascript:(function(d,u,s){s=d.createElement('script');s.type='text/javascript';s.charset='utf-8';s.src=u;d.body.appendChild(s)})(document,'https://cdn.rawgit.com/BloodedWine/Bookmarklet/master/shh.js')"
+});
 if (thing == "DONGLES") alert("iPhone 7 has many Dongles");
+// People
 if( thing.indexOf('CAIDEN') >= 0)alert("Caiden was here");
 if( thing.indexOf('JOEY') >= 0)alert("Joey was here");
-if( thing.indexOf('Q','P') >= 0)alert("Contains a Q or a P");
+// Censor
+if( thing.indexOf('DICK') >= 0)alert("Censored");
+if( thing.indexOf('FUCK') >= 0)alert("Censored");
+if( thing.indexOf('SHIT') >= 0)alert("Censored");
+if( thing.indexOf('ASS') >= 0)alert("Censored");
+// else
 else alert(ting);
